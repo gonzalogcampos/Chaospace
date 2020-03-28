e4b4d5440440bb0c4b2ee0f033b2c8d98fc07994
gogarcia <fxgxcx@hotmail.com>
2020-03-28 13:07:58 +0100

    gitignore

---
+++ CMakeFiles/Chaospace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chaospace.dir/main.cpp.o"
+  "bin/Chaospace.pdb"
+  "bin/Chaospace"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chaospace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/modulos.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/usuario/Documentos/Chaospace/sources/Components/Ai.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/Components/Ai.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/Components/Controller.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/Components/Controller.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/Components/Life.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/Components/Life.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GUI/Hud.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GUI/Hud.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GUI/Interface.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GUI/Interface.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/Game.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/Game.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GameObjects/Bullet.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GameObjects/Bullet.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GameObjects/GameObject.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GameObjects/GameObject.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GameObjects/Npc.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GameObjects/Npc.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GameObjects/Player.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GameObjects/Player.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GameObjects/PowerUp.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GameObjects/PowerUp.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/GameObjects/Ship.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/GameObjects/Ship.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/Map.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/Map.cpp.o"
+  "/home/usuario/Documentos/Chaospace/sources/State/PlayState.cpp" "/home/usuario/Documentos/Chaospace/CMakeFiles/modulos.dir/sources/State/PlayState.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "sources/Components"
+  "sources/GameObjects"
+  "sources/GUI"
+  "sources/State"
+  "sources"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
