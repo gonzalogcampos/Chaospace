d3292cb0c1d9f7d9307522912778ae69c2eae3bd
gogarcia <fxgxcx@hotmail.com>
2020-04-04 17:59:11 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
 #include <Game.h>
 
 int main() {
   Game::getInstance()->inicio();
   Game::getInstance()->run();
 
-  return EXIT_SUCCESS;
+  return 0;
 }
