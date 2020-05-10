7a61a6c0a7d5fab457dc2effe5d9864d3a16c07a
Alex <keyalex97@gmail.com>
2020-05-10 12:09:57 +0200

    Cambio power ups

--- sources/GameObjects/PowerUp.cpp
+++ sources/GameObjects/PowerUp.cpp
@@ -20,13 +20,12 @@ PowerUp::PowerUp(PowerUpType t, float x, float y) : GameObject()
         Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/energy.png", Rrect(50*3, 0, 50, 50)));
         Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/energy.png", Rrect(50*4, 0, 50, 50)));
         */
-        Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/health.png"));
-        Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/health2.png"));
+        Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/health/health.png"));
+        Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/health/health2.png"));
         break;
     case VELDIS:
         Render::getInstance()->addFrameToAnimation(animation, Render::getInstance()->createSprite("resources/att-speed.png"));
         break;
-    
 
     default:
         break;
