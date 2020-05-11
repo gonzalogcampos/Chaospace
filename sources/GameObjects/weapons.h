533a1b78cc0d6b62bdf6f0b47269deca5f9681e3
davidhl4 <dahella4@gmail.com>
2020-05-11 07:04:27 -0700

    Armas final

--- sources/GameObjects/weapons.h
+++ sources/GameObjects/weapons.h
@@ -2,18 +2,18 @@
 
 
 const float w1_Cadencia = 1.f;
-const float w1_Velocidad = 1000.f;
+const float w1_Velocidad = 800.f;
 const int w1_Fuerza = 10;
 
-const float w2_Cadencia = 1.f;
-const float w2_Velocidad = 800.f;
+const float w2_Cadencia = 0.9f;
+const float w2_Velocidad = 1000.f;
 const int w2_Fuerza = 10;
 
-const float w3_Cadencia = 1.f;
-const float w3_Velocidad = 500.f;
+const float w3_Cadencia = 0.8f;
+const float w3_Velocidad = 700.f;
 const int w3_Fuerza = 15;
 
-const float w4_Cadencia = 1.f;
+const float w4_Cadencia = 0.7f;
 const float w4_Velocidad = 1500.f;
 const int w4_Fuerza = 20;
 
